c81879c163cd14575a77a806bd61899c9636a9d6
Nathan Savard <savardnm@gmail.com>
2020-05-01 01:38:14 -0400

    create final output table

--- book-reduction.py
+++ book-reduction.py
@@ -9,6 +9,7 @@ Created on Thu Apr 30 14:27:55 2020
 import pandas as pd
 
 book_data = pd.read_csv('book_tags.csv')
+tag_data = pd.read_csv('final-output.csv')
 
 book_output = pd.DataFrame(index = [], columns = ['goodreads_book_id', 'tag_id', 'count'])
 
@@ -16,15 +17,17 @@ count = 0
 book_id = 0
 total = 0
 for index, row in book_data.iterrows():
-    count += 1
     total += 1
-    if((total%1000) == 0):
-        print(total)
-    if(count < 3 ):
-        book_output = book_output.append(row, ignore_index = True)
+    count += 1
     if(book_id != row['goodreads_book_id']):
-        count = -1
+        count = 0
         book_id = row['goodreads_book_id']
+    if((total%10000) == 0):
+        print(total)
+    if((row['tag_id'] in tag_data['tag_id'].values) and (count < 5)):
+        book_output = book_output.append(row, ignore_index = True)
+    #if(total > 1000):
+    #    break
 
 
 
--- table-generation.py
+++ table-generation.py
@@ -7,36 +7,42 @@ This is a temporary script file.
 import pandas as pd
 
 tag_data = pd.read_csv('final-output.csv')
-user_data = pd.read_csv('ratings-user-sorted.csv')
-book_data = pd.read_csv('book_tags.csv')
+user_data = pd.read_csv('ratings-2.csv')
+book_data = pd.read_csv('shortened-books.csv')
+id_data = pd.read_csv('books.csv')
 
+print(id_data.iloc[1-1]['book_id'])
 all_users = list(set([row['user_id'] for index, row in user_data.iterrows()]))
 
-output = pd.DataFrame(index = all_users, columns = [row['tag_id'] for index, row in tag_data.iterrows()])
-counts = pd.DataFrame(index = all_users, columns = [row['tag_id'] for index, row in tag_data.iterrows()])
+output = pd.DataFrame(0.1,index = all_users, columns = [row['tag_id'] for index, row in tag_data.iterrows()])
+counts = pd.DataFrame(0,index = all_users, columns = [row['tag_id'] for index, row in tag_data.iterrows()])
 total = 0
 for user_index, user_row in user_data.iterrows():
     user_id = user_row['user_id']
-    book_id = user_row['book_id']
+    book_id = id_data.iloc[user_row['book_id']-1]['book_id']
     rating = user_row['rating']
     count = 0
-    print(total)
-    
+    if((total%1000)==0):
+        print(total)
     total += 1
-    for book_index, book_row in book_data.iterrows():
-        if (book_id == book_row['goodreads_book_id']):
-            if (book_row['tag_id'] in output.columns):
-                output[book_row['tag_id']][user_id] += rating
-                counts[book_row['tag_id']][user_id] += 1.0
-            count = count + 1
-            if (count > 3):
-                break
+    done = False
+    
+    search = book_data.loc[lambda book_data: book_data['goodreads_book_id'] == book_id]
+    
+    for book_index, book_row in search.iterrows():
+        if (book_row['tag_id'] in output.columns):
+            output[book_row['tag_id']][user_id] += rating
+            counts[book_row['tag_id']][user_id] += 1.0
+    
+print(output)
+    
 
-for index, row in output:
-    for name, column in row:
-        output[index][name] = output[index][name] / counts[index][name]
-        
-
-output.to_csv("absolute-final.csv", index=True)
+for index, row in output.iterrows():
+    for name, column in row.iteritems():
+        if(output[name][index]==0.1):
+            output[name][index] = 3.0
+        else:
+            output[name][index] = (output[name][index]-0.1) / max(counts[name][index],1)
 
 print (output)
+output.to_csv("absolute-final-2.csv", index=True)
